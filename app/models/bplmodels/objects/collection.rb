426860afca953bfe06f60e78c1f30eb0d90f5d60
Steven Anderson <sanderson@bpl.org>
2012-09-19 15:18:32 -0400

    Added a module of 'Objects' to all of the fedora objects.

--- app/models/bplmodels/objects/collection.rb
+++ app/models/bplmodels/objects/collection.rb
@@ -1,28 +1,30 @@
 module Bplmodels
-  class Collection < ActiveFedora::Base
-    include Hydra::ModelMixins::CommonMetadata
-    include Hydra::ModelMethods
-    include ActiveFedora::Relationships
+  module Objects
+    class Collection < ActiveFedora::Base
+      include Hydra::ModelMixins::CommonMetadata
+      include Hydra::ModelMethods
+      include ActiveFedora::Relationships
 
-    #has_relationship "similar_audio", :has_part, :type=>AudioRecord
+      #has_relationship "similar_audio", :has_part, :type=>AudioRecord
 
-    has_metadata :name => "descMetadata", :type => Datastreams::ModsDescMetadata
+      has_metadata :name => "descMetadata", :type => Datastreams::ModsDescMetadata
 
-    delegate :title, :to=>'descMetadata', :at => [:mods, :titleInfo, :title], :unique=>true
-    delegate :abstract, :to => "descMetadata"
-    delegate :url, :to=>'descMetadata', :at => [:relatedItem, :location, :url], :unique=>true
-    delegate :description, :to=>'descMetadata', :at => [:abstract], :unique=>true
-    delegate :location, :to=>'descMetadata', :at => [:relatedItem, :location, :physicalLocation], :unique=>true
-    delegate :identifier_accession, :to=>'descMetadata', :at => [:identifier_accession], :unique=>true
-    delegate :identifier_barcode, :to=>'descMetadata', :at => [:identifier_barcode], :unique=>true
-    delegate :identifier_bpldc, :to=>'descMetadata', :at => [:identifier_bpldc], :unique=>true
-    delegate :identifier_other, :to=>'descMetadata', :at => [:identifier_other], :unique=>true
+      delegate :title, :to=>'descMetadata', :at => [:mods, :titleInfo, :title], :unique=>true
+      delegate :abstract, :to => "descMetadata"
+      delegate :url, :to=>'descMetadata', :at => [:relatedItem, :location, :url], :unique=>true
+      delegate :description, :to=>'descMetadata', :at => [:abstract], :unique=>true
+      delegate :location, :to=>'descMetadata', :at => [:relatedItem, :location, :physicalLocation], :unique=>true
+      delegate :identifier_accession, :to=>'descMetadata', :at => [:identifier_accession], :unique=>true
+      delegate :identifier_barcode, :to=>'descMetadata', :at => [:identifier_barcode], :unique=>true
+      delegate :identifier_bpldc, :to=>'descMetadata', :at => [:identifier_bpldc], :unique=>true
+      delegate :identifier_other, :to=>'descMetadata', :at => [:identifier_other], :unique=>true
 
-    def apply_default_permissions
-      self.datastreams["rightsMetadata"].update_permissions( "group"=>{"Repository Administrators"=>"edit"} )
-      self.datastreams["rightsMetadata"].update_permissions( "group"=>{"Repository Administrators"=>"read"} )
-      self.datastreams["rightsMetadata"].update_permissions( "group"=>{"Repository Administrators"=>"discover"} )
-      self.save
+      def apply_default_permissions
+        self.datastreams["rightsMetadata"].update_permissions( "group"=>{"Repository Administrators"=>"edit"} )
+        self.datastreams["rightsMetadata"].update_permissions( "group"=>{"Repository Administrators"=>"read"} )
+        self.datastreams["rightsMetadata"].update_permissions( "group"=>{"Repository Administrators"=>"discover"} )
+        self.save
+      end
     end
   end
 end
