bcc1cc9130d96b801bbcf2a5a34fa937c65740e5
Eben English <eenglish@bpl.org>
2013-08-29 11:33:06 -0400

    add pubplace_tsim field for place of publication data

--- app/models/bplmodels/simple_object_base.rb
+++ app/models/bplmodels/simple_object_base.rb
@@ -188,6 +188,8 @@ module Bplmodels
 
       doc['publisher_tsim'] = self.descMetadata.origin_info.publisher
 
+      doc['pubplace_tsim'] = self.descMetadata.origin_info.place
+
       doc['lang_term_ssim'] = self.descMetadata.language.language_term
       #doc['lang_val_uri_ssim'] = self.descMetadata.language.language_term.lang_val_uri
 
