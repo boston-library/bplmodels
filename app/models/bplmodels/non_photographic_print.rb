a1adb874cd51fcb96871a145a822c455db60c10f
Steven Anderson <sanderson@bpl.org>
2013-09-11 11:09:07 -0400

    Fixed two incorrect definitions in models

--- app/models/bplmodels/non_photographic_print.rb
+++ app/models/bplmodels/non_photographic_print.rb
@@ -2,11 +2,6 @@ module Bplmodels
   class NonPhotographicPrint < Bplmodels::SimpleObjectBase
     #has_file_datastream :name => 'productionMaster', :type => ActiveFedora::Datastream
 
-    belongs_to :institution, :class_name => 'Bplmodels::Institution', :property => :is_member_of
-
-    has_many :image_files, :class_name => "Bplmodels::ImageFile", :property=> :has_image
-
-
     #A collection can have another collection as a member, or an image
     def insert_member(fedora_object)
       if (fedora_object.instance_of?(Bplmodels::ImageFile))
