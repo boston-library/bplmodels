35d35530530b6356c813a7f4143c2b2e6deb591c
Eben English <eenglish@bpl.org>
2013-10-30 11:50:01 -0400

    convert_to_mods_dates() method added for converting date data from OAI feeds into MODS-usable date data

--- bplmodels.gemspec
+++ bplmodels.gemspec
@@ -18,6 +18,7 @@ Gem::Specification.new do |s|
 
   s.add_dependency "rails"
   s.add_dependency "mods"
+  s.add_dependency "timeliness"
   # s.add_dependency "jquery-rails"
 
   s.add_development_dependency "sqlite3"
--- lib/bplmodels.rb
+++ lib/bplmodels.rb
@@ -1,5 +1,11 @@
 require "bplmodels/engine"
 require "bplmodels/datastream_input_funcs"
+require "timeliness"
+
+# add some formats to Timeliness gem for better parsing
+Timeliness.add_formats(:date, 'm-d-yy', :before => 'd-m-yy')
+Timeliness.add_formats(:date, 'mmm[\.]? d[a-z]?[a-z]?[,]? yyyy')
+Timeliness.add_formats(:date, 'yyyy mmm d')
 
 module Bplmodels
 end
