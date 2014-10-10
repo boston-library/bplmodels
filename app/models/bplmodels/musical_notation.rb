8ddbb1f08e738218b13ffae7cda1fe4ab630d9e4
Steven Anderson <sanderson@bpl.org>
2014-10-10 16:51:02 -0400

    Added a content model for musical notation

---
+++ app/models/bplmodels/musical_notation.rb
@@ -0,0 +1,34 @@
+module Bplmodels
+  class MusicalNotation < Bplmodels::SimpleObjectBase
+    #has_file_datastream :name => 'productionMaster', :type => ActiveFedora::Datastream
+
+
+    #A collection can have another collection as a member, or an image
+    def insert_member(fedora_object)
+      if (fedora_object.instance_of?(Bplmodels::ImageFile))
+
+        #add to the members ds
+        members.insert_member(:member_id=>fedora_object.pid, :member_title=>fedora_object.titleSet_display, :member_type=>fedora_object.fedora_name)
+
+        #add to the rels-ext ds
+        fedora_object.object << self
+        self.image_files << fedora_object
+
+      end
+
+      fedora_object.save!
+      self.save!
+
+    end
+
+    def fedora_name
+      'musical_notation'
+    end
+
+    def to_solr(doc = {} )
+      doc = super(doc)
+      doc['active_fedora_model_ssi'] = self.class.name
+      doc
+    end
+  end
+end
