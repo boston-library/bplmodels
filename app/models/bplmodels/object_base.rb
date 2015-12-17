de534eaf12a89d42c16cf0d732856c82223e165b
Steven Anderson <sanderson@bpl.org>
2015-12-17 15:04:59 -0500

    Update note to save on remove_oai_relationships

--- app/models/bplmodels/object_base.rb
+++ app/models/bplmodels/object_base.rb
@@ -84,7 +84,6 @@ module Bplmodels
 
     def remove_oai_relationships
       self.remove_relationship(:oai_item_id, "oai:digitalcommonwealth.org:" + self.pid, true)
-      self.save
     end
 
     #alias :limited_delete :delete
