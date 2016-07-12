31a89629561275f0922e97420e6b22b9242af401
Eben English <eenglish@bpl.org>
2016-07-12 15:43:37 -0400

    updating Collection and OAICollection to_solr method for genre values

--- app/models/bplmodels/oai_collection.rb
+++ app/models/bplmodels/oai_collection.rb
@@ -5,6 +5,18 @@ module Bplmodels
     end
     # To change this template use File | Settings | File Templates.
 
+    def to_solr(doc = {} )
+      super(doc)
+
+      exemplary_check = Bplmodels::OAIObject.find_with_conditions({"is_exemplary_image_of_ssim"=>"info:fedora/#{self.pid}"}, rows: '1', fl: 'id' )
+      if exemplary_check.present?
+        doc['exemplary_image_ssi'] = exemplary_check.first["id"]
+      end
+
+      doc
+
+    end
+
     #Expects the following args:
     #parent_pid => id of the parent object
     #local_id => local ID of the object
