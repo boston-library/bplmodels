93f021a548e29e21b8d8026e7cfa176fdcfe3b9e
Eben English <eenglish@bpl.org>
2017-03-20 15:00:55 -0400

    make filenames searchable

--- app/models/bplmodels/file.rb
+++ app/models/bplmodels/file.rb
@@ -59,6 +59,11 @@ module Bplmodels
 
     def to_solr(doc = {} )
       doc = super(doc)
+
+      doc['label_ssi'] = self.label.to_s
+      doc['filename_ssi'] = self.filename.first
+      doc['filename_base_ssi'] = self.filename.first.gsub(/\.[a-z]*\z/,'')
+
       if self.workflowMetadata.marked_for_deletion.present?
         doc['marked_for_deletion_bsi']  =  self.workflowMetadata.marked_for_deletion.first
         doc['marked_for_deletion_reason_ssi']  =  self.workflowMetadata.marked_for_deletion.reason.first
